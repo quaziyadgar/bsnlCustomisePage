e95e45f080ca69e80cba3b22680baa262bece148
quaziyadgar <quaziyadgar@gmail.com>
2024-08-20 14:54:39 +0530

    some fixes

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 // let download_app_close_btn = document.getElementById("download_app_close_btn");
 
 function handleDownload(value) {
-    console.log(value);
+  console.log(value);
   let element = document.getElementById("emergency-trigger-panel");
   if (value) element.style = "display: block !important";
   else element.style = "display: none !important";
 }
 
-// download_app_open_btn.addEventListener(()=>{
-//     element.style.display = "block";
-// })
-
-// download_app_close_btn.addEventListener(()=>{
-//     element.style.display = "none";
-// })
+function handleBadgeColor(value) {
+  document.getElementById("badge1").style.background = "#010c3a";
+  document.getElementById("badge2").style.background = "#010c3a";
+  document.getElementById("badge3").style.background = "#010c3a";
+  let element = document.getElementById(value);
+  element.style.background = "#df0303";
+}
+handleBadgeColor('badge1');  //default call for badge 1
